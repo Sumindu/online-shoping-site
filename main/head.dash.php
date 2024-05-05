2a3a58f01ca0301936df0b9de6f1307e4f2ef75a
Sumindu Prabhath <bsumindu@gmail.com>
2024-05-05 21:43:39 +0530

    config icos

--- main/head.dash.php
+++ main/head.dash.php
@@ -39,22 +39,29 @@
             </div>
         </div>
         <ul class="sidebar-list">
+            <li class="sidebar-list-item <?php echo ($currentPage == '.php') ? 'active' : ''; ?>">
+                <a href="../index.html">
+                    <span>Home</span>
+                </a>
+            </li>
             <li class="sidebar-list-item <?php echo ($currentPage == 'shop.php') ? 'active' : ''; ?>">
                 <a href="shop.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
                     <span>Shop</span>
                 </a>
             </li>
             <li class="sidebar-list-item <?php echo ($currentPage == 'cart.php') ? 'active' : ''; ?>">
                 <a href="cart.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/><line x1="3" y1="6" x2="21" y2="6"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
                     <span>Cart</span>
                 </a>
             </li>
             <li class="sidebar-list-item <?php echo ($currentPage == 'feedback.php') ? 'active' : ''; ?>">
                 <a href="feedback.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-inbox"><polyline points="22 12 16 12 14 15 10 15 8 12 2 12"/><path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"/></svg>
                     <span>Feedback</span>
+                </a>
+            </li>
+            <li class="sidebar-list-item <?php echo ($currentPage == '.php') ? 'active' : ''; ?>">
+                <a href="../guestabout.html">
+                    <span>About Us</span>
                 </a>
             </li>
         </ul>
